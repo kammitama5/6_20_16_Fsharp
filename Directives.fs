5a655e24434e06a3881ce947c6f828aff09faf5d
kammitama5 <krystal.maughan@gmail.com>
2016-06-20 12:55:15 -0700

    second commit

---
+++ Directives.fs
@@ -0,0 +1,16 @@
+﻿// Set the compilation warning to light mode; pronounced "Hash Light".
+#light
+
+//Open directives
+open System
+
+//Output Hello World
+Console.WriteLine("hello World")
+Console.WriteLine("hello World")
+Console.WriteLine("hello World")
+Console.WriteLine("hello World")
+
+
+
+//Stop the console from closing
+Console.ReadKey(true)
